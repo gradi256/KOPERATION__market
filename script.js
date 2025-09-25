0b12cd49664f73fad2f7b683f20a38f0bf6015ed
Gradi Nawej <gradinawej8@gmail.com>
2025-09-25 22:11:21 +0100

    images rajoutés dans la section hero

--- script.js
+++ script.js
@@ -9,3 +9,32 @@ btnMenuFermeture.addEventListener("click",() => {
     let navbar = document.querySelector(".navbar");
     navbar.classList.remove("navbar2");
 })
+
+
+// hero
+
+// tableau des images
+const imagesHero = [
+    "images/hero/1.jpeg",
+    "images/hero/2.jpeg",
+    "images/hero/3.jpeg",
+    "images/hero/4.jpeg",
+    "images/hero/5.jpeg",
+    "images/hero/6.jpeg"
+];
+
+let indexHero = 0;
+const heroslider = document.getElementById("heroSlider");
+
+// fonction pour changer l'image
+function changeHeroImgage() {
+    indexHero++;
+    if(indexHero >= imagesHero.length) indexHero = 0;
+    heroslider.style.opacity = 0; 
+    setTimeout(() => {
+        heroslider.src = imagesHero[indexHero];
+        heroslider.style.opacity = 1;
+    }, 300);
+}
+
+setInterval(changeHeroImgage,3000);
